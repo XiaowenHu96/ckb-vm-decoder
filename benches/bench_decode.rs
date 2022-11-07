12810bfe5ccd997cde93b2fe9ae44d812b2e311e
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2022-11-07 14:18:57 +0800

    add i type

--- benches/bench_decode.rs
+++ benches/bench_decode.rs
@@ -1,84 +1,66 @@
 #[macro_use]
 extern crate criterion;
 
-use ckb_vm::instructions::{b, v};
-use ckb_vm_decoder::{rvb_decoder, rvv_decoder};
+use ckb_vm::instructions::{b, i, v};
+use ckb_vm_decoder::{rvb_decoder, rvi_decoder, rvv_decoder};
 use criterion::{BenchmarkId, Criterion};
 use rand::prelude::*;
 
-pub fn bench_rvv_decode(c: &mut Criterion) {
-    let gen_inst = |idx: usize| -> u32 {
-        let i = idx % rvv_decoder::INSTRUCTION_LIST.len();
-        rvv_decoder::INSTRUCTION_LIST[i].get_match_bits()
+macro_rules! bench_rand_decode {
+    ($name:literal, $hash_based:ident, $pattern_based:ident) => {
+        pub fn $hash_based(c: &mut Criterion) {
+            let gen_inst = |idx: usize| -> u32 {
+                let i = idx % $hash_based::INSTRUCTION_LIST.len();
+                $hash_based::INSTRUCTION_LIST[i].get_match_bits()
+            };
+
+            const ITERATION: usize = 100_000_000;
+            let mut group = c.benchmark_group("group");
+            group.sample_size(10);
+
+            let seed = rand::thread_rng().gen();
+            let mut rng0 = SmallRng::from_seed(seed);
+            let mut rng1 = SmallRng::from_seed(seed);
+
+            group.bench_function(BenchmarkId::new(concat!($name, "-pattern-based"), 0), |b| {
+                b.iter(|| {
+                    for _ in 0..ITERATION {
+                        let idx: usize = rng0.gen();
+                        let bits = gen_inst(idx);
+                        $pattern_based::factory::<u64>(bits, 0);
+                    }
+                })
+            });
+
+            group.bench_function(BenchmarkId::new(concat!($name, "-hash-based"), 0), |b| {
+                b.iter(|| {
+                    for _ in 0..ITERATION {
+                        let idx: usize = rng1.gen();
+                        let bits = gen_inst(idx);
+                        $hash_based::factory::<u64>(bits, 0);
+                    }
+                })
+            });
+            group.finish();
+        }
     };
-
-    const ITERATION: usize = 100_000_000;
-    let mut group = c.benchmark_group("group");
-    group.sample_size(10);
-
-    let seed = rand::thread_rng().gen();
-    let mut rng0 = SmallRng::from_seed(seed);
-    let mut rng1 = SmallRng::from_seed(seed);
-
-    group.bench_function(BenchmarkId::new("rvv-match-based", 0), |b| {
-        b.iter(|| {
-            for _ in 0..ITERATION {
-                let idx: usize = rng0.gen();
-                let bits = gen_inst(idx);
-                v::factory::<u64>(bits, 0);
-            }
-        })
-    });
-
-    group.bench_function(BenchmarkId::new("rvv-hash-based", 0), |b| {
-        b.iter(|| {
-            for _ in 0..ITERATION {
-                let idx: usize = rng1.gen();
-                let bits = gen_inst(idx);
-                rvv_decoder::factory::<u64>(bits, 0);
-            }
-        })
-    });
-
-    group.finish();
 }
 
-pub fn bench_rvb_decode(c: &mut Criterion) {
-    let gen_inst = |idx: usize| -> u32 {
-        let i = idx % rvb_decoder::INSTRUCTION_LIST.len();
-        rvb_decoder::INSTRUCTION_LIST[i].get_match_bits()
-    };
-
-    const ITERATION: usize = 100_000_000;
-    let mut group = c.benchmark_group("group");
-    group.sample_size(10);
-
-    let seed = rand::thread_rng().gen();
-    let mut rng0 = SmallRng::from_seed(seed);
-    let mut rng1 = SmallRng::from_seed(seed);
-
-    group.bench_function(BenchmarkId::new("rvb-match-based", 0), |b| {
-        b.iter(|| {
-            for _ in 0..ITERATION {
-                let idx: usize = rng0.gen();
-                let bits = gen_inst(idx);
-                b::factory::<u64>(bits, 0);
-            }
-        })
-    });
-
-    group.bench_function(BenchmarkId::new("rvb-hash-based", 0), |b| {
-        b.iter(|| {
-            for _ in 0..ITERATION {
-                let idx: usize = rng1.gen();
-                let bits = gen_inst(idx);
-                rvb_decoder::factory::<u64>(bits, 0);
-            }
-        })
-    });
-
-    group.finish();
+macro_rules! bench_groups {
+    ($( ($name: literal, $hash_based:ident, $pattern_based:ident )),*) => {
+        $(
+            bench_rand_decode!($name, $hash_based, $pattern_based);
+        )*
+        criterion_group!(
+            benches,
+            $( $hash_based),*
+        );
+    }
 }
 
-criterion_group!(benches, bench_rvv_decode, bench_rvb_decode);
+bench_groups!(
+    ("rvv", rvv_decoder, v),
+    ("rvb", rvb_decoder, b),
+    ("rvi", rvi_decoder, i)
+);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
 include!("./mod.rs");
 use ckb_vm::instructions::insts;
 
-use ckb_vm::instructions::{Instruction, Itype, Rtype, VItype, VVtype, VXtype};
+use ckb_vm::instructions::{
+    blank_instruction, Instruction, Itype, Rtype, Stype, Utype, VItype, VVtype, VXtype,
+};
+
+// TODO xiaowen: quick hack, fix API
+// The FENCE instruction is used to order device I/O and memory accesses
+// as viewed by other RISC- V harts and external devices or coprocessors.
+#[derive(Debug, Clone, Copy)]
+pub struct FenceType(Instruction);
+
+impl FenceType {
+    pub fn new(fm: u8, pred: u8, succ: u8) -> Self {
+        FenceType(Rtype::new(insts::OP_FENCE, fm as usize, pred as usize, succ as usize).0)
+    }
+
+    pub fn fm(self) -> u8 {
+        Rtype(self.0).rd() as u8
+    }
+
+    pub fn pred(self) -> u8 {
+        Rtype(self.0).rs1() as u8
+    }
+
+    pub fn succ(self) -> u8 {
+        Rtype(self.0).rs2() as u8
+    }
+}
 
 pub type OpcodeBuilder = fn(instruction_bits: u32, insts::InstructionOpcode) -> Instruction;
 
@@ -39,6 +65,11 @@ pub fn x(instruction_bits: u32, lower: usize, length: usize, shifts: usize) -> u
 }
 
 #[inline(always)]
+pub fn xs(instruction_bits: u32, lower: usize, length: usize, shifts: usize) -> u32 {
+    ((instruction_bits as i32) << (32 - lower - length) >> (32 - length) << shifts) as u32
+}
+
+#[inline(always)]
 pub fn funct3(instruction_bits: u32) -> u32 {
     x(instruction_bits, 12, 3, 0)
 }
@@ -63,8 +94,103 @@ pub fn rs2(instruction_bits: u32) -> usize {
     x(instruction_bits, 20, 5, 0) as usize
 }
 
+#[inline(always)]
+pub fn utype_immediate(instruction_bits: u32) -> i32 {
+    xs(instruction_bits, 12, 20, 12) as i32
+}
+
+#[inline(always)]
+pub fn itype_immediate(instruction_bits: u32) -> i32 {
+    xs(instruction_bits, 20, 12, 0) as i32
+}
+
+#[inline(always)]
+pub fn btype_immediate(instruction_bits: u32) -> i32 {
+    (x(instruction_bits, 8, 4, 1)
+        | x(instruction_bits, 25, 6, 5)
+        | x(instruction_bits, 7, 1, 11)
+        | xs(instruction_bits, 31, 1, 12)) as i32
+}
+
+#[inline(always)]
+pub fn stype_immediate(instruction_bits: u32) -> i32 {
+    (x(instruction_bits, 7, 5, 0) | xs(instruction_bits, 25, 7, 5)) as i32
+}
+
 fn vm(instruction_bits: u32) -> bool {
     instruction_bits & 0x2000000 != 0
+}
+
+pub fn us_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Utype::new_s(
+        opcode,
+        rd(instruction_bits),
+        utype_immediate(instruction_bits),
+    )
+    .0
+}
+
+pub fn is_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Itype::new_s(
+        opcode,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        itype_immediate(instruction_bits),
+    )
+    .0
+}
+
+pub fn is_alu_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Itype::new_s(
+        opcode,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        //TODO xiaowen: Just to compile, fix this
+        itype_immediate(instruction_bits) & i32::from(10),
+    )
+    .0
+}
+pub fn is_1f_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Itype::new_s(
+        opcode,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        itype_immediate(instruction_bits) & 0x1F,
+    )
+    .0
+}
+
+pub fn sb_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Stype::new_s(
+        opcode,
+        btype_immediate(instruction_bits),
+        rs1(instruction_bits),
+        rs2(instruction_bits),
+    )
+    .0
+}
+
+pub fn ss_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Stype::new_s(
+        opcode,
+        stype_immediate(instruction_bits),
+        rs1(instruction_bits),
+        rs2(instruction_bits),
+    )
+    .0
+}
+
+pub fn blank_inst_builder(_: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    blank_instruction(opcode)
+}
+
+pub fn fencei_builder(instruction_bits: u32, _: insts::InstructionOpcode) -> Instruction {
+    FenceType::new(
+        ((instruction_bits & 0xF00_00000) >> 28) as u8,
+        ((instruction_bits & 0x0F0_00000) >> 24) as u8,
+        ((instruction_bits & 0x00F_00000) >> 20) as u8,
+    )
+    .0
 }
 
 pub fn r_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
