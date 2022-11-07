8d7a775895acd0e819a46febf716953b163f0c43
XiaowenHu96 <xihu5895@uni.sydney.edu.au>
2022-11-07 11:28:41 +0800

    add b instruction set

--- benches/bench_decode.rs
+++ benches/bench_decode.rs
@@ -1,17 +1,17 @@
 #[macro_use]
 extern crate criterion;
 
-use ckb_vm::instructions::v;
-use ckb_vm_decoder::rvv_decoder;
+use ckb_vm::instructions::{b, v};
+use ckb_vm_decoder::{rvb_decoder, rvv_decoder};
 use criterion::{BenchmarkId, Criterion};
 use rand::prelude::*;
 
-fn gen_inst(idx: usize) -> u32 {
-    let i = idx % rvv_decoder::INSTRUCTION_LIST.len();
-    rvv_decoder::INSTRUCTION_LIST[i].match_bits
-}
+pub fn bench_rvv_decode(c: &mut Criterion) {
+    let gen_inst = |idx: usize| -> u32 {
+        let i = idx % rvv_decoder::INSTRUCTION_LIST.len();
+        rvv_decoder::INSTRUCTION_LIST[i].get_match_bits()
+    };
 
-pub fn bench_rand_decode(c: &mut Criterion) {
     const ITERATION: usize = 100_000_000;
     let mut group = c.benchmark_group("group");
     group.sample_size(10);
@@ -20,7 +20,7 @@ pub fn bench_rand_decode(c: &mut Criterion) {
     let mut rng0 = SmallRng::from_seed(seed);
     let mut rng1 = SmallRng::from_seed(seed);
 
-    group.bench_function(BenchmarkId::new("if-else-based", 0), |b| {
+    group.bench_function(BenchmarkId::new("rvv-match-based", 0), |b| {
         b.iter(|| {
             for _ in 0..ITERATION {
                 let idx: usize = rng0.gen();
@@ -30,7 +30,7 @@ pub fn bench_rand_decode(c: &mut Criterion) {
         })
     });
 
-    group.bench_function(BenchmarkId::new("hash-based", 0), |b| {
+    group.bench_function(BenchmarkId::new("rvv-hash-based", 0), |b| {
         b.iter(|| {
             for _ in 0..ITERATION {
                 let idx: usize = rng1.gen();
@@ -43,5 +43,42 @@ pub fn bench_rand_decode(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_rand_decode);
+pub fn bench_rvb_decode(c: &mut Criterion) {
+    let gen_inst = |idx: usize| -> u32 {
+        let i = idx % rvb_decoder::INSTRUCTION_LIST.len();
+        rvb_decoder::INSTRUCTION_LIST[i].get_match_bits()
+    };
+
+    const ITERATION: usize = 100_000_000;
+    let mut group = c.benchmark_group("group");
+    group.sample_size(10);
+
+    let seed = rand::thread_rng().gen();
+    let mut rng0 = SmallRng::from_seed(seed);
+    let mut rng1 = SmallRng::from_seed(seed);
+
+    group.bench_function(BenchmarkId::new("rvb-match-based", 0), |b| {
+        b.iter(|| {
+            for _ in 0..ITERATION {
+                let idx: usize = rng0.gen();
+                let bits = gen_inst(idx);
+                b::factory::<u64>(bits, 0);
+            }
+        })
+    });
+
+    group.bench_function(BenchmarkId::new("rvb-hash-based", 0), |b| {
+        b.iter(|| {
+            for _ in 0..ITERATION {
+                let idx: usize = rng1.gen();
+                let bits = gen_inst(idx);
+                rvb_decoder::factory::<u64>(bits, 0);
+            }
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_rvv_decode, bench_rvb_decode);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,35 @@ use ckb_vm::instructions::insts;
 
 use ckb_vm::instructions::{Instruction, Itype, Rtype, VItype, VVtype, VXtype};
 
+pub type OpcodeBuilder = fn(instruction_bits: u32, insts::InstructionOpcode) -> Instruction;
+
+pub struct InstructionInfo {
+    mask: u32,
+    match_bits: u32,
+    opcode: insts::InstructionOpcode,
+    builder: OpcodeBuilder,
+}
+
+impl InstructionInfo {
+    pub const fn new(
+        mask: u32,
+        match_bits: u32,
+        opcode: insts::InstructionOpcode,
+        builder: OpcodeBuilder,
+    ) -> Self {
+        InstructionInfo {
+            mask,
+            match_bits,
+            opcode,
+            builder,
+        }
+    }
+
+    pub const fn get_match_bits(&self) -> u32 {
+        self.match_bits
+    }
+}
+
 // TODO Xiaowen: quick hack
 #[inline(always)]
 pub fn x(instruction_bits: u32, lower: usize, length: usize, shifts: usize) -> u32 {
@@ -36,6 +65,37 @@ pub fn rs2(instruction_bits: u32) -> usize {
 
 fn vm(instruction_bits: u32) -> bool {
     instruction_bits & 0x2000000 != 0
+}
+
+pub fn r_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Rtype::new(
+        opcode,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        rs2(instruction_bits),
+    )
+    .0
+}
+
+// TODO Better name
+pub fn r64_imm_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
+    Itype::new_u(
+        opcode,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        x(instruction_bits, 20, 6, 0),
+    )
+    .0
+}
+
+pub fn roriw_builder(instruction_bits: u32, _: insts::InstructionOpcode) -> Instruction {
+    Itype::new_u(
+        insts::OP_SLLIUW,
+        rd(instruction_bits),
+        rs1(instruction_bits),
+        x(instruction_bits, 20, 5, 0),
+    )
+    .0
 }
 
 pub fn vx_builder(instruction_bits: u32, opcode: insts::InstructionOpcode) -> Instruction {
